ce59d5f9a7f37f94ea771e900522c7f9e0e1e93a
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-24 02:20:59 +0300

    added route to view form

---
+++ Wildlife-tracker/src/main/java/App.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        //HEROKU
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+
+        get("/About", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/About.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
