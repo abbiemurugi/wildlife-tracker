6cbdbc61affbcfb09f32773cd0314b46ea3ebc4c
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-24 02:30:33 +0300

    added route to post animal details

--- Wildlife-tracker/src/main/java/App.java
+++ Wildlife-tracker/src/main/java/App.java
@@ -26,3 +26,28 @@ public class App {
             model.put("template", "templates/About.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+        get("/About", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/About.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+        post("/addAnimal", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String ranger = request.queryParams("ranger");
+            String age = request.queryParams("age");
+            String location = request.queryParams("location");
+            String health = request.queryParams("health");
+            String status = request.queryParams("status");
+            Animal newAnimal = new Animal(name, ranger, age, location, health, status);
+            newAnimal.save();
+            response.redirect("/");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+    }
+
+}
